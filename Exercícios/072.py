1492cec6a0b65a1f95d14bbcd45bd67bd025161c
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-23 11:01:55 -0300

    Resolução do exercício 072

--- Exercícios/072.py
+++ Exercícios/072.py
@@ -1,3 +1,12 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por
 # extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20 e mostrá-lo por extenso.
+
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete','dezoito', 'dezenove', 'vinte')
+
+numero = int(input('Digite um número entre 0 e 20: '))
+
+while numero < 0 or numero > 20:
+   numero = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+
+print(f'Você digitou o número {numeros[numero]}.')
