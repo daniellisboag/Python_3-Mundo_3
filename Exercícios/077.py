a52f4d707465088957e93d770a98a6324e3373f1
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-24 10:48:03 -0300

    Resolução do exercício 077

--- Exercícios/077.py
+++ Exercícios/077.py
@@ -1,2 +1,10 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos.)
-# Depois disso, você deve mostrar, para cada palavram, quais são as suas vogais.
+# Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+
+palavras = ('Aprender', 'Programar', 'Linguagem', 'Python', 'Curso', 'Gratis', 'Estudar', 'Praticar', 'Trabalhar', 'Mercado', 'Programador', 'Futuro')
+
+for c in palavras:
+   print(f'\nNa palavra {c} temos ', end = '')
+   for vogal in c:
+      if vogal.upper() in 'AEIOU':
+         print(vogal.upper(), end = ' ')
