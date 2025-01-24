0bfdffab8f30a459bb654564a56f33b6ab8d73df
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-24 09:22:08 -0300

    Resolução do exercício 073

--- Exercícios/073.py
+++ Exercícios/073.py
@@ -4,3 +4,16 @@
 # b) Os últimos 4 colocados da tabela.
 # c) Uma lista com todos os times em ordem alfabética.
 # d) Em que posição na tabela está o time da Chapecoense.
+
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo', 'Atlético - PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport Recife', 'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético - GO')
+
+print('-' * 40)
+print(f'Lista de times do brasileirão: {times}')
+print('-' * 40)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-' * 40)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-' * 40)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-' * 40)
+print(f'O Chapecoense está na {times.index('Chapecoense') + 1}° posição.')
