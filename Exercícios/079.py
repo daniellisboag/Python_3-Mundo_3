e14402e73f0869daff024efbdc5a0539dd9f602d
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-28 10:50:01 -0300

    Resolução do exercício 079

--- Exercícios/079.py
+++ Exercícios/079.py
@@ -1,3 +1,21 @@
-# Crie um programa onde o usuário posa digitar vários valores numéricos e cadastre-os
+# Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os
 # em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final,
 # serão exibidos todos os valores únicos digitados, em ordem crescente.
+
+lista = []
+
+while True:
+   valor = int(input('Digite um valor: '))
+   if valor in lista:
+      print('Valor duplicado. Não vou adicionar na lista!')
+   else:
+      lista.append(valor)
+   opcao = input('Quer continuar? [S/N] ').strip().upper()[0]
+   while opcao not in 'SN':
+      print('Opção inválida. Tente novamente!')
+      opcao = input('Quer continuar? [S/N] ').strip().upper()[0]
+   if opcao == 'N':
+      break
+print('-' * 40)
+lista.sort()
+print(f'Você digitou os valores {lista}')
