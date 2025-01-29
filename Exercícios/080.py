c38c0c805e6c4d98e8dabf5c3bb5785f33b0afad
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-29 09:13:32 -0300

    Resolução do exercício 080

--- Exercícios/080.py
+++ Exercícios/080.py
@@ -1,3 +1,22 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
 # já na posição correta de inserção (sem usar o sort()).
 # No final, mostre a lista ordenada na tela.
+
+lista = []
+
+for c in range(1, 6):
+   valor = int(input('Digite um valor: '))
+   if c == 1 or valor > lista[-1]:
+      lista.append(valor)
+      print('Adicionado ao final da lista.')
+   else:
+      pos = 0
+      while pos < len(lista):
+         if valor <= lista[pos]:
+            lista.insert(pos, valor)
+            print(f'Adicionado na posição {pos} da lista.')
+            break
+         pos += 1
+
+print(f'Os valores digitados em ordem foram: {lista}')
+print(len(lista))
