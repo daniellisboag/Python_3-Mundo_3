f35f5208190d16d2ed984f0578e64ae260e486e9
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-29 10:10:03 -0300

    Resolução do exercício 083

--- Exercícios/083.py
+++ Exercícios/083.py
@@ -1,3 +1,21 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos
 # e fechados na ordem correta.
+
+expressao = input('Digite a expressão: ')
+lista = []
+
+for c in expressao:
+   if c == '(':
+      lista.append('(')
+   elif c == ')':
+      if len(lista) > 0:
+         lista.pop()
+      else:
+         lista.append(')')
+         break
+
+if len(lista) == 0:
+   print('Sua expressão está válida!')
+else:
+   print('Sua expressão está inválida!')
