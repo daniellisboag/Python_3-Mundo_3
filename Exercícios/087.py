87810336425d5979b4756040e9d098d983783c78
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-31 10:45:07 -0300

    Exercícios do 084 ao 089

---
+++ Exercícios/087.py
@@ -0,0 +1,4 @@
+# Aprimore o desafio anterior, mostrando no final:
+# a) A soma de todos o valores pares digitador.
+# b) A soma dos valores da terceira coluna.
+# c) O maior valor da segunda linha.
