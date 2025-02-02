3575099b9c790884b7ebeb1a2611c4ec5d7fa12e
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-02 16:35:21 -0300

    Resolução do exercício 086

--- Exercícios/086.py
+++ Exercícios/086.py
@@ -1,2 +1,13 @@
 # Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 # No final, mostre a matriz na tela, com a formatação correta.
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for c in range(0, 3):
+   for i in range(0, 3):
+      matriz[c][i] = int(input(f'Digite um valor para [{c}, {i}]: '))
+
+for c in range(0, 3):
+   for i in range(0, 3):
+      print(f'[{matriz[c][i]:^5}]', end = '')
+   print()
