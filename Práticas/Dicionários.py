dc2d218e98784d5eb6eeded6acd4efd127dbb44c
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-04 14:13:35 -0300

    Dicionários

--- Práticas/Dicionários.py
+++ Práticas/Dicionários.py
@@ -3,3 +3,6 @@ filme = {'titulo' : 'Star Wars', 'Ano' : 1977, 'Diretor' : 'George Lucas'}
 print(filme.values())
 print(filme.keys())
 print(filme.items())
+
+for k, v in filme.items():
+   print(f'O {k} é {v}')
