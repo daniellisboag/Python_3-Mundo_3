056522cbdb2b32f4bf86a41625c8800e79eef3ec
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-05 10:54:48 -0300

    Dicionários

--- Práticas/Dicionários.py
+++ Práticas/Dicionários.py
@@ -1,3 +1,4 @@
+# Cria um dicionário.
 filme = {'titulo' : 'Star Wars', 'Ano' : 1977, 'Diretor' : 'George Lucas'}
 
 print(filme.values())
@@ -6,3 +7,48 @@ print(filme.items())
 
 for k, v in filme.items():
    print(f'O {k} é {v}')
+
+# ---------------------------------------------------------
+
+# Cria um dicionário.
+pessoas = {'nome' : 'Daniel', 'sexo' : 'M', 'idade': 24}
+
+print(pessoas['nome'])
+print(f'O {pessoas['nome']} tem {pessoas['idade']} anos.')
+
+# Mostra as chaves.
+print(pessoas.keys())
+
+# Mostra os valores.
+print(pessoas.values())
+
+# Mostra os itens.
+print(pessoas.items())
+
+# Apaga elemento 'sexo'.
+del pessoas['sexo']
+
+# Modifica o elemento 'nome'.
+pessoas['nome'] = 'Gustavo'
+
+# Adiciona um elemento ao dicionário
+pessoas['peso'] = 65.5
+
+for keys, values in pessoas.items():
+   print(f'{keys} = {values}')
+
+# ---------------------------------------------------------
+
+# Cria uma lista
+brasil = []
+
+# Cria dois dicionários
+estado_1 = {'uf' : 'Rio de Janeiro', 'sigla' : 'RJ'}
+estado_2 = {'uf' : 'São Paulo', 'sigla' : 'SP'}
+
+# Adiciona os dois dicionários na lista
+brasil.append(estado_1)
+brasil.append(estado_2)
+
+# Mostra o elemento 'uf' do primeiro dicionário na lista.
+print(brasil[0]['uf'])
