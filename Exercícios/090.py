f39bf31b60aa2ae4ec65e487aabc913bd5c255ed
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-06 10:06:48 -0300

    Exercícios do 090 ao 096

---
+++ Exercícios/090.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia o nome e média de um aluno, guardando também a situação
+# em um dicionário. No final, mostre o conteúdo da estrutura na tela.
---
+++ Exercícios/096.py
@@ -0,0 +1,2 @@
+# Aprimore o desafio 093 para que ele funciona com vários jogadores, incluindo
+# um sistema de visualização de detalhes do aproveitamento de cada jogador.
