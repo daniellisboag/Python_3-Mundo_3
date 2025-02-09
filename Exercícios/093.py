10fa6d27ad4be47bf514b054e1d664857e63f30e
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-08 22:33:48 -0300

    Resolução do exercício 093

--- Exercícios/093.py
+++ Exercícios/093.py
@@ -2,3 +2,29 @@
 # ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols
 # feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o
 # total de gols feitos durante o campeonato.
+
+dados = {}
+gols = []
+
+dados['Nome'] = input('Nome do jogador: ')
+partidas = int(input(f'Quantas partidas {dados['Nome']} jogou? '))
+
+for c in range(0, partidas):
+   gols.append(int(input(f'Quantos gols na {c + 1}° partida? ')))
+
+dados['Gols'] = gols
+dados['Total'] = sum(gols)
+
+print('-=' * 20)
+print(dados)
+print('-=' * 20)
+
+for k, v in dados.items():
+   print(f'{k}: {v}')
+
+print('-=' * 20)
+print(f'O jogador {dados['Nome']} jogou {partidas} partidas.')
+
+for c, i in enumerate(gols):
+   print(f'Na {c + 1}° partida fez {i} gols.')
+print(f'Total de gols: {dados['Total']}')
