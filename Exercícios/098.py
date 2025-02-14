49ca0d6f621e1e65fd73b9c4ceae670b5f9cee92
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-02-14 12:21:57 -0300

    Resolução do exercício 098

--- Exercícios/098.py
+++ Exercícios/098.py
@@ -4,3 +4,29 @@
 # a) De 1 até 10, de 1 em 1
 # b) De 10 até 0, de 2 em 2
 # c) uma contagem personalizada.
+
+from time import sleep
+
+def contagem(inicio, fim, passo):
+   if passo < 0:
+      passo = abs(passo)
+   elif passo == 0:
+      passo = 1
+   print('-' * 35)
+   print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+   if inicio < fim:
+      for c in range(inicio, fim + 1, passo):
+         print(c, end = ' ')
+         sleep(0.2)
+   else:
+      for c in range(inicio, fim - 1, -passo):
+         print(c, end = ' ')
+         sleep(0.2)
+   print('Fim!')
+
+contagem(1, 10, 1)
+contagem(10, 0, 2)
+
+print('-' * 35)
+print('Agora é sua vez de personalizar a contagem!')
+contagem(int(input('Início: ')), int(input('Fim: ')), int(input('Passo: ')))
