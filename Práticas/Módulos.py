3164ac881a7338515bf2255d6773f9a56fc3cd23
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-05-17 14:21:58 -0300

    Revert "Modularização em python"
    This reverts commit be40f8920e783d8d55fce313a6607c3a63ecc829.

--- Práticas/Módulos.py
+++
@@ -1,7 +0,0 @@
-from Uteis import Números
-
-num = int(input('Digite um valor: '))
-fat = Números.fatorial(num)
-print(f'O fatorial de {num} é: {fat}')
-print(f'O dobro de {num} é: {Números.dobro(num)}')
-print(f'O triplo de {num} é: {Números.triplo(num)}')
